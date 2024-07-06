cd407a594233e0a4c8bdde9572bf82d9c89bf4d7
szawrowski <alexander.szawrowski@gmail.com>
2024-07-07 01:10:49 +0300

    feat: Add file stream definitions

---
+++ include/caitlyn/core/io/defs/file_definitions.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_CORE_IO_FILE_DEFINITIONS_H_
+#define CAITLYN_CORE_IO_FILE_DEFINITIONS_H_
+
+#include <fstream>
+#include <streambuf>
+
+#include "caitlyn/core/defs/basic_types.h"
+
+BEGIN_CAITLYN_NS
+
+template <typename CharT>
+using std_basic_ifstream = std::basic_ifstream<CharT>;
+using ifile_t = std_basic_ifstream<u8char_t>;
+using xifile_t = std_basic_ifstream<xchar_t>;
+
+template <typename CharT>
+using std_basic_ofstream = std::basic_ofstream<CharT>;
+using ofile_t = std_basic_ofstream<u8char_t>;
+using xofile_t = std_basic_ofstream<xchar_t>;
+
+template <typename CharT>
+using std_basic_fstream = std::basic_fstream<CharT>;
+using file_t = std_basic_fstream<u8char_t>;
+using xfile_t = std_basic_fstream<xchar_t>;
+
+template <typename CharT>
+using std_basic_filebuf = std::basic_filebuf<CharT>;
+using filebuf_t = std_basic_filebuf<u8char_t>;
+using xfilebuf_t = std_basic_filebuf<xchar_t>;
+
+END_CAITLYN_NS
+
+#endif // CAITLYN_CORE_IO_FILE_DEFINITIONS_H_
