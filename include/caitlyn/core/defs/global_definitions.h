1304354be1491ce909a14b6b9bc7b6e0f63a27e2
szawrowski <alexander.szawrowski@gmail.com>
2024-07-06 03:30:42 +0300

    feat: Add global namespace macro

---
+++ include/caitlyn/core/defs/global_definitions.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_CORE_DEFS_GLOBAL_DEFINITIONS_H_
+#define CAITLYN_CORE_DEFS_GLOBAL_DEFINITIONS_H_
+
+#define BEGIN_CAITLYN_NS namespace cait {
+#define END_CAITLYN_NS }
+
+#endif  // CAITLYN_CORE_DEFS_GLOBAL_DEFINITIONS_H_
