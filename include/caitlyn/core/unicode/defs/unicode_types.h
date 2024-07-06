13df759d1af560b5df12ab0e4291d849f22490d0
szawrowski <alexander.szawrowski@gmail.com>
2024-07-06 03:31:03 +0300

    feat: Add Unicode code point type

---
+++ include/caitlyn/core/unicode/defs/unicode_types.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_CORE_UNICODE_DEFS_UNICODE_TYPES_H_
+#define CAITLYN_CORE_UNICODE_DEFS_UNICODE_TYPES_H_
+
+#include "caitlyn/core/defs/basic_types.h"
+
+BEGIN_CAITLYN_NS
+
+using code_point_t = uint32_t;
+
+END_CAITLYN_NS
+
+#endif  // CAITLYN_CORE_UNICODE_DEFS_UNICODE_TYPES_H_
