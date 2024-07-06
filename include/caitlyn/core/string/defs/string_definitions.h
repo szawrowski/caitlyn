bafe76d1eb79aec749a1ac397650a3b9208f4272
szawrowski <alexander.szawrowski@gmail.com>
2024-07-07 01:10:19 +0300

    feat: Update string definitions

--- include/caitlyn/core/string/defs/string_definitions.h
+++ include/caitlyn/core/string/defs/string_definitions.h
@@ -14,16 +14,31 @@ BEGIN_CAITLYN_NS
 
 template <typename CharT>
 using std_basic_string = std::basic_string<CharT>;
-
 using std_string_t = std_basic_string<u8char_t>;
 using std_xstring_t = std_basic_string<xchar_t>;
 using std_u8string_t = std_basic_string<u8char_t>;
 using std_u16string_t = std_basic_string<u16char_t>;
 using std_u32string_t = std_basic_string<u32char_t>;
 
-using istrstream_t = std::basic_istringstream<u8char_t>;
-using ostrstream_t = std::basic_ostringstream<u8char_t>;
-using strstream_t = std::basic_stringstream<u8char_t>;
+template <typename CharT>
+using std_basic_strview = std::basic_string_view<CharT>;
+using std_strview_t = std_basic_strview<u8char_t>;
+using std_xstrview_t = std_basic_strview<xchar_t>;
+using std_u8strview_t = std_basic_strview<u8char_t>;
+using std_u16strview_t = std_basic_strview<u16char_t>;
+using std_u32strview_t = std_basic_strview<u32char_t>;
+
+template <typename CharT>
+using std_basic_istringstream = std::basic_istringstream<CharT>;
+using istrstream_t = std_basic_istringstream<u8char_t>;
+
+template <typename CharT>
+using std_basic_ostringstream = std::basic_ostringstream<CharT>;
+using ostrstream_t = std_basic_ostringstream<u8char_t>;
+
+template <typename CharT>
+using std_basic_stringstream = std::basic_stringstream<CharT>;
+using strstream_t = std_basic_stringstream<u8char_t>;
 
 END_CAITLYN_NS
 
