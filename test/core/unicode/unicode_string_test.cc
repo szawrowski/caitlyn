4c9d01c4799b4dd3fd502c2c45b0e045dd1f4188
szawrowski <alexander.szawrowski@gmail.com>
2024-07-07 11:08:02 +0300

    test: Update Unicode string tests

--- test/core/unicode/unicode_string_test.cc
+++ test/core/unicode/unicode_string_test.cc
@@ -3,28 +3,28 @@
 #include "caitlyn/core/string.h"
 
 TEST(CoreUnicodeTest, StringDefaultInit) {
-  const cait::unistring_t str{};
+  const cait::unistr_t str{};
   ASSERT_TRUE(str == "");
 }
 
 TEST(CoreUnicodeTest, StringOneCharInit) {
-  const cait::unistring_t str{"A"};
+  const cait::unistr_t str{"A"};
   ASSERT_TRUE(str == "A");
 }
 
 TEST(CoreUnicodeTest, StringInit) {
-  const cait::unistring_t str{"Some string. 🙂"};
+  const cait::unistr_t str{"Some string. 🙂"};
   ASSERT_TRUE(str == "Some string. 🙂");
 }
 
 TEST(CoreUnicodeTest, StringAssignmentOperator) {
-  cait::unistring_t str{};
+  cait::unistr_t str{};
   str = "Some string. 🙂";
   ASSERT_TRUE(str == "Some string. 🙂");
 }
 
 TEST(CoreUnicodeTest, OutputStreamOperator) {
-  const cait::unistring_t str = "Some string. 🙂";
+  const cait::unistr_t str = "Some string. 🙂";
   std::basic_ostringstream<cait::u8char_t> oss;
   oss << str;
 
