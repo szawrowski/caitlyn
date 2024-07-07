a58a6d05f1c5f6066ebe99d37ebfc2d908f5ac17
szawrowski <alexander.szawrowski@gmail.com>
2024-07-07 10:38:04 +0300

    feat: Update file definitions

--- include/caitlyn/core/io/defs/file_definitions.h
+++ include/caitlyn/core/io/defs/file_definitions.h
@@ -13,24 +13,24 @@
 BEGIN_CAITLYN_NS
 
 template <typename CharT>
-using std_basic_ifstream = std::basic_ifstream<CharT>;
-using ifile_t = std_basic_ifstream<u8char_t>;
-using xifile_t = std_basic_ifstream<xchar_t>;
+using basic_ifile_t = std::basic_ifstream<CharT>;
+using ifile_t = basic_ifile_t<u8char_t>;
+using xifile_t = basic_ifile_t<xchar_t>;
 
 template <typename CharT>
-using std_basic_ofstream = std::basic_ofstream<CharT>;
-using ofile_t = std_basic_ofstream<u8char_t>;
-using xofile_t = std_basic_ofstream<xchar_t>;
+using basic_ofile_t = std::basic_ofstream<CharT>;
+using ofile_t = basic_ofile_t<u8char_t>;
+using xofile_t = basic_ofile_t<xchar_t>;
 
 template <typename CharT>
-using std_basic_fstream = std::basic_fstream<CharT>;
-using file_t = std_basic_fstream<u8char_t>;
-using xfile_t = std_basic_fstream<xchar_t>;
+using basic_file_t = std::basic_fstream<CharT>;
+using file_t = basic_file_t<u8char_t>;
+using xfile_t = basic_file_t<xchar_t>;
 
 template <typename CharT>
-using std_basic_filebuf = std::basic_filebuf<CharT>;
-using filebuf_t = std_basic_filebuf<u8char_t>;
-using xfilebuf_t = std_basic_filebuf<xchar_t>;
+using basic_filebuf = std::basic_filebuf<CharT>;
+using filebuf_t = basic_filebuf<u8char_t>;
+using xfilebuf_t = basic_filebuf<xchar_t>;
 
 END_CAITLYN_NS
 
