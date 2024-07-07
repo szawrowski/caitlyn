6571a442a0986d65d28a349b02dc8fd25f426954
szawrowski <alexander.szawrowski@gmail.com>
2024-07-07 09:14:31 +0300

    feat: Add char utility functions

---
+++ include/caitlyn/core/char/utility/char_utility.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_CORE_CHAR_UTILITY_CHAR_UTILITY_H_
+#define CAITLYN_CORE_CHAR_UTILITY_CHAR_UTILITY_H_
+
+#include <cctype>
+
+#include "caitlyn/core/defs/basic_types.h"
+
+BEGIN_CAITLYN_NS
+
+static constexpr auto is_space(const stdchar_t value) {
+  return value == '\x0A' || value == '\x0B' || value == '\x0C' ||
+         value == '\x0D' || value == '\x20';
+}
+
+static constexpr auto is_uppercase(const stdchar_t value) {
+  return value >= 0x41 && value < 0x5B;
+}
+
+static constexpr auto is_lowercase(const stdchar_t value) {
+  return value >= 0x61 && value < 0x7B;
+}
+
+static constexpr auto is_alpha(const stdchar_t value) {
+  return is_uppercase(value) || is_lowercase(value);
+}
+
+static constexpr auto is_digit(const stdchar_t value) {
+  return value >= 0x30 && value < 0x3A;
+}
+
+static constexpr auto to_uppercase(const stdchar_t value) {
+  const auto tmp = static_cast<uchar_t>(value);
+  return static_cast<stdchar_t>(std::toupper(tmp));
+}
+
+static constexpr auto to_lowercase(const stdchar_t value) {
+  const auto tmp = static_cast<uchar_t>(value);
+  return static_cast<stdchar_t>(std::tolower(tmp));
+}
+
+END_CAITLYN_NS
+
+#endif  // CAITLYN_CORE_CHAR_UTILITY_CHAR_UTILITY_H_
