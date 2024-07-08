7e521c4c16a2b4f5f2b6ab5ed34c32cb2b94d1d7
szawrowski <alexander.szawrowski@gmail.com>
2024-07-09 02:49:45 +0300

    feat: Add format definitions

---
+++ include/caitlyn/core/string/format/defs/format_definitions.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_CORE_STRING_FORMAT_DEFS_FORMAT_DEFINITIONS_H_
+#define CAITLYN_CORE_STRING_FORMAT_DEFS_FORMAT_DEFINITIONS_H_
+
+#include "caitlyn/core/core.h"
+
+#define __caitlyn_begin_format_namespace namespace format {
+#define __caitlyn_end_format_namespace }
+
+#endif  // CAITLYN_CORE_STRING_FORMAT_DEFS_FORMAT_DEFINITIONS_H_
