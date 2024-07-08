f5e3750360ab8c29716d78c41539437e55ed8ed1
szawrowski <alexander.szawrowski@gmail.com>
2024-07-08 04:35:52 +0300

    fix: Update include paths

---
+++ include/caitlyn/core/unicode/types/types.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_CORE_UNICODE_TYPES_TYPES_H_
+#define CAITLYN_CORE_UNICODE_TYPES_TYPES_H_
+
+#include "caitlyn/core/unicode/types/unicode_char.h"
+#include "caitlyn/core/unicode/types/unicode_file_reader.h"
+#include "caitlyn/core/unicode/types/unicode_file_writer.h"
+#include "caitlyn/core/unicode/types/unicode_string.h"
+
+#endif  // CAITLYN_CORE_UNICODE_TYPES_TYPES_H_
