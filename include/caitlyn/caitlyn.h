e9e0b3174bca43363d686d5598c157abe464fadd
szawrowski <alexander.szawrowski@gmail.com>
2024-07-08 04:36:34 +0300

    feat: Add library core headers

---
+++ include/caitlyn/caitlyn.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_CAITLYN_H_
+#define CAITLYN_CAITLYN_H_
+
+#include "caitlyn/core.h"
+#include "caitlyn/ser.h"
+
+#endif  // CAITLYN_CAITLYN_H_
