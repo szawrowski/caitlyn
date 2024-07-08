bcb2e848dd757c7cd28ab32ef603a4f6b3a3bfea
szawrowski <alexander.szawrowski@gmail.com>
2024-07-08 04:25:46 +0300

    feat: Add global serializing definitions

---
+++ include/caitlyn/ser/defs/global_definitions.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2024, Alexander Szawrowski
+//
+// This file is distributed under the MIT License.
+// See LICENSE file for details.
+
+#ifndef CAITLYN_SER_DEFS_GLOBAL_DEFINITIONS_H_
+#define CAITLYN_SER_DEFS_GLOBAL_DEFINITIONS_H_
+
+#include "caitlyn/core/core.h"
+
+#define BEGIN_JSON_NS namespace json {
+#define END_JSON_NS }
+
+#endif  // CAITLYN_SER_DEFS_GLOBAL_DEFINITIONS_H_
