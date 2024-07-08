b98f250e426263516942d73f422373349a048077
szawrowski <alexander.szawrowski@gmail.com>
2024-07-08 06:37:00 +0300

    feat: Add make error utility functions

--- include/caitlyn/core/error/types/error.h
+++ include/caitlyn/core/error/types/error.h
@@ -6,7 +6,7 @@
 #ifndef CAITLYN_CORE_ERROR_TYPES_ERROR_H_
 #define CAITLYN_CORE_ERROR_TYPES_ERROR_H_
 
-#include "caitlyn/core/core.h"
+#include <caitlyn/core/string/defs/string_definitions.h>
 
 BEGIN_CAITLYN_NS
 
@@ -14,22 +14,37 @@ template <typename E>
 class error_t {
 public:
   explicit error_t(E error) : error_{std::move(error)} {}
-  const E& get() const noexcept { return error_; }
+  [[nodiscard]] const E& get() const noexcept { return error_; }
 
 private:
   E error_;
 };
 
 template <typename E>
-bool operator==(const error_t<E>& lhs, const error_t<E>& rhs) {
+static error_t<E> make_error(E error) {
+  return error_t<E>(error);
+}
+
+static error_t<string_t> make_error(const char_t* error) {
+  return error_t(string_t{error});
+}
+
+static error_t<string_t> make_error(const string_t& error) {
+  return error_t(error);
+}
+
+END_CAITLYN_NS
+
+template <typename E>
+bool operator==(const cait::error_t<E>& lhs, const cait::error_t<E>& rhs) {
   return lhs.get() == rhs.get();
 }
 
 template <typename E>
-bool operator!=(const error_t<E>& lhs, const error_t<E>& rhs) {
+bool operator!=(const cait::error_t<E>& lhs, const cait::error_t<E>& rhs) {
   return !(lhs == rhs);
 }
 
-END_CAITLYN_NS
+
 
 #endif  // CAITLYN_CORE_ERROR_TYPES_ERROR_H_
