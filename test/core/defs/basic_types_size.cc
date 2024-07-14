a82d4b7357a07b0cd6d6f42697cbdd22c08f40aa
szawrowski <alexander.szawrowski@gmail.com>
2024-07-14 11:57:25 +0300

    test: Update basic types test

--- test/core/defs/basic_types_size.cc
+++ test/core/defs/basic_types_size.cc
@@ -2,7 +2,6 @@
 
 TEST(CoreDefsTest, BasicTypesSizeUnix) {
 #if defined(__caitlyn_unix) && (__caitlyn_arch == 64)
-  ASSERT_EQ(sizeof(cait::char_t), 1);
   ASSERT_EQ(sizeof(cait::schar_t), 1);
   ASSERT_EQ(sizeof(cait::uchar_t), 1);
   ASSERT_EQ(sizeof(cait::u8char_t), 1);
@@ -10,23 +9,10 @@ TEST(CoreDefsTest, BasicTypesSizeUnix) {
   ASSERT_EQ(sizeof(cait::u32char_t), 4);
   ASSERT_EQ(sizeof(cait::byte_t), 1);
   ASSERT_EQ(sizeof(cait::sbyte_t), 1);
-  ASSERT_EQ(sizeof(cait::int_t), 4);
-  ASSERT_EQ(sizeof(cait::uint_t), 4);
-  ASSERT_EQ(sizeof(cait::int8_t), 1);
-  ASSERT_EQ(sizeof(cait::uint8_t), 1);
-  ASSERT_EQ(sizeof(cait::int16_t), 2);
-  ASSERT_EQ(sizeof(cait::uint16_t), 2);
-  ASSERT_EQ(sizeof(cait::int32_t), 4);
-  ASSERT_EQ(sizeof(cait::uint32_t), 4);
-  ASSERT_EQ(sizeof(cait::int64_t), 8);
-  ASSERT_EQ(sizeof(cait::uint64_t), 8);
-  ASSERT_EQ(sizeof(cait::ptrdiff_t), 8);
-  ASSERT_EQ(sizeof(cait::size_t), 8);
   ASSERT_EQ(sizeof(cait::ssize_t), 8);
   ASSERT_EQ(sizeof(cait::float32_t), 4);
   ASSERT_EQ(sizeof(cait::float64_t), 8);
   ASSERT_EQ(sizeof(cait::floatx_t), 16);
-  ASSERT_EQ(sizeof(cait::null_t), 8);
 #endif
 }
 
