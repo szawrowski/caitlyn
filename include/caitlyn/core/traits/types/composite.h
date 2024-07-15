16be7604c4a8077ecf5622a78667edcfd94af2b5
szawrowski <alexander.szawrowski@gmail.com>
2024-07-15 15:37:08 +0300

    feat: Add composite type constraints

---
+++ include/caitlyn/core/traits/types/composite.h
@@ -0,0 +1,79 @@
+/**
+ * This file is part of the Caitlyn library
+ * Copyright (C) 2024 Alexander Szawrowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CAITLUN_CORE_TRAITS_TYPES_COMPOSITE_H_
+#define CAITLUN_CORE_TRAITS_TYPES_COMPOSITE_H_
+
+#include "caitlyn/core/traits/types/primary.h"
+
+namespace cait {
+namespace traits {
+
+template <typename T>
+struct is_arithmetic_t : bool_constant_t<is_integral<T>() || is_floating<T>()> {
+};
+
+template <typename T>
+struct is_fundamental_t : bool_constant_t<is_arithmetic_t<T>::value ||
+                                          is_void<T>() || is_null_ptr<T>()> {};
+
+template <class T>
+struct is_scalar_t
+    : bool_constant_t<is_arithmetic_t<T>::value || is_enum<T>() ||
+                      is_ptr<T>() || std::is_member_pointer<T>::value ||
+                      is_null_ptr<T>()> {};
+
+}  // namespace traits
+
+template <typename T>
+constexpr bool is_fundamental() {
+  return traits::is_fundamental_t<T>::value;
+}
+
+template <typename T>
+constexpr bool is_arithmetic() {
+  return traits::is_arithmetic_t<T>::value;
+}
+
+template <typename T>
+constexpr bool is_scalar() {
+  return traits::is_scalar_t<T>::value;
+}
+
+template <typename T>
+constexpr bool is_object() {
+  return std::is_object<T>::value;
+}
+
+template <typename T>
+constexpr bool is_compound() {
+  return std::is_compound<T>::value;
+}
+
+template <typename T>
+constexpr bool is_ref() {
+  return std::is_reference<T>::value;
+}
+
+template <typename T>
+constexpr bool is_member_ptr() {
+  return std::is_member_pointer<T>::value;
+}
+
+}  // namespace cait
+
+#endif  // CAITLUN_CORE_TRAITS_TYPES_COMPOSITE_H_
