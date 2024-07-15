ebf862d07a8430089d463305397f1ebbf24686ce
szawrowski <alexander.szawrowski@gmail.com>
2024-07-15 15:37:44 +0300

    feat: Add type properties constraints

---
+++ include/caitlyn/core/traits/types/properties.h
@@ -0,0 +1,87 @@
+/**
+ * This file is part of the Caitlyn library
+ * Copyright (C) 2024 Alexander Szawrowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CAITLUN_CORE_TRAITS_TYPES_PROPERTIES_H_
+#define CAITLUN_CORE_TRAITS_TYPES_PROPERTIES_H_
+
+#include "caitlyn/core/traits/types/bool_constant.h"
+#include "caitlyn/core/traits/types/composite.h"
+
+namespace cait {
+namespace traits {
+
+template <typename T, bool = is_arithmetic<T>()>
+struct is_signed_t : bool_constant_t<T(-1) < T(0)> {};
+
+template <typename T, bool = is_arithmetic<T>()>
+struct is_unsigned_t : bool_constant_t<T(0) < T(-1)> {};
+
+}  // namespace traits
+
+template <typename T>
+constexpr bool is_const() {
+  return std::is_const<T>::value;
+}
+
+template <typename T>
+constexpr bool is_volatile() {
+  return std::is_volatile<T>::value;
+}
+
+template <typename T>
+constexpr bool is_trivial() {
+  return std::is_trivial<T>::value;
+}
+
+template <typename T>
+constexpr bool is_trivially_copyable() {
+  return std::is_trivially_copyable<T>::value;
+}
+
+template <typename T>
+constexpr bool is_standard_layout() {
+  return std::is_standard_layout<T>::value;
+}
+
+template <typename T>
+constexpr bool is_empty() {
+  return std::is_empty<T>::value;
+}
+
+template <typename T>
+constexpr bool is_polymorphic() {
+  return std::is_polymorphic<T>::value;
+}
+
+template <typename T>
+constexpr bool is_abstract() {
+  return std::is_abstract<T>::value;
+}
+
+template <typename T>
+constexpr bool is_signed() {
+  return traits::is_signed_t<T>::value;
+}
+
+template <typename T>
+constexpr bool is_unsigned() {
+  return traits::is_unsigned_t<T>::value;
+}
+
+}  // namespace cait
+
+#endif  // CAITLUN_CORE_TRAITS_TYPES_PROPERTIES_H_
