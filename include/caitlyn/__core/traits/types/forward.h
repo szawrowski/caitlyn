7ccabd3d3edd73e0aaaba7127a143699547ce430
szawrowski <alexander.szawrowski@gmail.com>
2024-07-21 05:37:16 +0300

    feat: Add forward functions

---
+++ include/caitlyn/__core/traits/types/forward.h
@@ -0,0 +1,39 @@
+/**
+ * This file is part of the Caitlyn library
+ * Copyright (C) 2024 Alexander Szawrowski
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef CAITLUN_CORE_TRAITS_TYPES_FORWARD_H_
+#define CAITLUN_CORE_TRAITS_TYPES_FORWARD_H_
+
+#include "caitlyn/__core/traits/types/references.h"
+
+__CAITLYN_GLOBAL_NAMESPACE_BEGIN
+
+template <typename T>
+T&& forward(typename std::remove_reference<T>::type& t) noexcept {
+  return static_cast<T&&>(t);
+}
+
+// Версия для rvalue-ссылок
+template <typename T>
+T&& forward(typename std::remove_reference<T>::type&& t) noexcept {
+  static_assert(!std::is_lvalue_reference<T>::value, "bad forward call");
+  return static_cast<T&&>(t);
+}
+
+__CAITLYN_GLOBAL_NAMESPACE_END
+
+#endif  // CAITLUN_CORE_TRAITS_TYPES_FORWARD_H_
